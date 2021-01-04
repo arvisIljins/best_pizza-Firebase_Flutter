f198e87a8095c8a56fd9e1ef6d3602aa46454933
yourgurunet <info@yourgurunet.com>
2021-01-04 11:08:16 +0200

    Basic App structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:firbase_example/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Retun either Home or Athenticate wiged
+    return Home();
+  }
+}
