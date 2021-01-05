d8d9f9f9f5103b21a7dd695159ab56c9d518f052
yourgurunet <info@yourgurunet.com>
2021-01-05 14:39:39 +0200

    loading animation with flutter_spinkit

--- lib/screens/authenticate/signIn.dart
+++ lib/screens/authenticate/signIn.dart
@@ -1,5 +1,6 @@
 import 'package:firbase_example/services/auth.dart';
 import 'package:firbase_example/shared/constants.dart';
+import 'package:firbase_example/shared/loading.dart';
 
 import 'package:flutter/material.dart';
 
@@ -19,91 +20,98 @@ class _SignInState extends State<SignIn> {
   String email = '';
   String password = '';
   String error = '';
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey[100],
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[400],
-        title: Text('Sign in - Best Pizza',
-            style: TextStyle(
-              color: Colors.blueGrey[100],
-            )),
-        actions: [
-          FlatButton(
-            onPressed: () {
-              widget.toggleShowSignIn();
-            },
-            child: Text(
-              'Sign Up',
-              style: TextStyle(
-                color: Colors.blueGrey[100],
-              ),
-            ),
-          ),
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                  cursorColor: Colors.blueGrey[400],
-                  decoration:
-                      textInputDecorations.copyWith(labelText: 'Your email'),
-                  onChanged: (val) {
-                    setState(() => email = val);
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.blueGrey[100],
+            appBar: AppBar(
+              backgroundColor: Colors.blueGrey[400],
+              title: Text('Sign in - Best Pizza',
+                  style: TextStyle(
+                    color: Colors.blueGrey[100],
+                  )),
+              actions: [
+                FlatButton(
+                  onPressed: () {
+                    widget.toggleShowSignIn();
                   },
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.blueGrey[100],
+                    ),
+                  ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  validator: (val) => val.isEmpty ? 'Enter an password' : null,
-                  cursorColor: Colors.blueGrey[400],
-                  decoration:
-                      textInputDecorations.copyWith(labelText: 'Your password'),
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                RaisedButton(
-                  color: Colors.blueGrey[400],
-                  child: Text('Sign in',
-                      style: TextStyle(
-                        color: Colors.blueGrey[100],
-                      )),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _auth.signInWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() =>
-                            error = 'Please check your email and password');
-                      }
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 12,
-                ),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 15),
-                )
               ],
             ),
-          )),
-    );
+            body: Container(
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an email' : null,
+                        cursorColor: Colors.blueGrey[400],
+                        decoration: textInputDecorations.copyWith(
+                            labelText: 'Your email'),
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an password' : null,
+                        cursorColor: Colors.blueGrey[400],
+                        decoration: textInputDecorations.copyWith(
+                            labelText: 'Your password'),
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      RaisedButton(
+                        color: Colors.blueGrey[400],
+                        child: Text('Sign in',
+                            style: TextStyle(
+                              color: Colors.blueGrey[100],
+                            )),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .signInWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() => error =
+                                  'Please check your email and password');
+                              loading = false;
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 15),
+                      )
+                    ],
+                  ),
+                )),
+          );
   }
 }
--- lib/screens/authenticate/userRegister.dart
+++ lib/screens/authenticate/userRegister.dart
@@ -1,5 +1,6 @@
 import 'package:firbase_example/services/auth.dart';
 import 'package:firbase_example/shared/constants.dart';
+import 'package:firbase_example/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class UserRegister extends StatefulWidget {
@@ -18,92 +19,99 @@ class _UserRegisterState extends State<UserRegister> {
   String email = '';
   String password = '';
   String error = '';
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey[100],
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[400],
-        title: Text('Sign Up - Best Pizza',
-            style: TextStyle(
-              color: Colors.blueGrey[100],
-            )),
-        actions: [
-          FlatButton(
-            onPressed: () {
-              widget.toggleShowSignIn();
-            },
-            child: Text(
-              'Sign In',
-              style: TextStyle(
-                color: Colors.blueGrey[100],
-              ),
-            ),
-          ),
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                  cursorColor: Colors.blueGrey[400],
-                  decoration:
-                      textInputDecorations.copyWith(labelText: 'Your email'),
-                  onChanged: (val) {
-                    setState(() => email = val);
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.blueGrey[100],
+            appBar: AppBar(
+              backgroundColor: Colors.blueGrey[400],
+              title: Text('Sign Up - Best Pizza',
+                  style: TextStyle(
+                    color: Colors.blueGrey[100],
+                  )),
+              actions: [
+                FlatButton(
+                  onPressed: () {
+                    widget.toggleShowSignIn();
                   },
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: Colors.blueGrey[100],
+                    ),
+                  ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  validator: (val) => val.length <= 6
-                      ? 'Password needs to be at least 6 characters long'
-                      : null,
-                  cursorColor: Colors.blueGrey[400],
-                  decoration:
-                      textInputDecorations.copyWith(labelText: 'Your password'),
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                RaisedButton(
-                  color: Colors.blueGrey[400],
-                  child: Text('Register',
-                      style: TextStyle(
-                        color: Colors.blueGrey[100],
-                      )),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _auth.registerWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() => error = 'Please supply a valid email');
-                      }
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 12,
-                ),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 15),
-                )
               ],
             ),
-          )),
-    );
+            body: Container(
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an email' : null,
+                        cursorColor: Colors.blueGrey[400],
+                        decoration: textInputDecorations.copyWith(
+                            labelText: 'Your email'),
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        validator: (val) => val.length <= 6
+                            ? 'Password needs to be at least 6 characters long'
+                            : null,
+                        cursorColor: Colors.blueGrey[400],
+                        decoration: textInputDecorations.copyWith(
+                            labelText: 'Your password'),
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      RaisedButton(
+                        color: Colors.blueGrey[400],
+                        child: Text('Register',
+                            style: TextStyle(
+                              color: Colors.blueGrey[100],
+                            )),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .registerWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(
+                                  () => error = 'Please supply a valid email');
+                              loading = false;
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 15),
+                      )
+                    ],
+                  ),
+                )),
+          );
   }
 }
