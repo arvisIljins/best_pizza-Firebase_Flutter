3247931770eac75d8e1cf153df9b261a4ea60ebe
yourgurunet <info@yourgurunet.com>
2021-01-05 06:13:40 +0200

    Setting up firebase stream

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -10,6 +10,11 @@ class AuthService {
     return user != null ? User(uid: user.uid) : null;
   }
 
+  //auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFirebaseUser);
+  }
+
   // sign in with email & password
   Future signInAnon() async {
     try {
