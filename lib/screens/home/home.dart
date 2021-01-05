85c29051b726051934a1f5345728293a94adeb5d
yourgurunet <info@yourgurunet.com>
2021-01-05 08:27:23 +0200

    Added logout button

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home'),
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[400],
+        title: Text('Best Pizza'),
+        actions: [
+          FlatButton.icon(
+            onPressed: () {},
+            label: Text('Logout'),
+            icon: Icon(Icons.login),
+          ),
+        ],
+      ),
     );
   }
 }
