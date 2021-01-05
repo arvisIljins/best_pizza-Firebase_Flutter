a4678bd386f261b87a9f2503f9dc62ef9252c951
yourgurunet <info@yourgurunet.com>
2021-01-05 06:29:47 +0200

    Adding Provider Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:firbase_example/screens/wrapper.dart';
+import 'package:firbase_example/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'modals/user.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,8 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,14 @@
+import 'package:firbase_example/modals/user.dart';
 import 'package:firbase_example/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
+
     //Retun either Home or Athenticate wiged
     return Authenticate();
   }
