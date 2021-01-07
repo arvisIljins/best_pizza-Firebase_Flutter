785486842de9ea22b0e6df383c3de69d53e6e836
yourgurunet <info@yourgurunet.com>
2021-01-07 09:35:52 +0200

    Get user doc stream

--- lib/modals/user.dart
+++ lib/modals/user.dart
@@ -3,3 +3,20 @@ class User {
 
   User({this.uid});
 }
+
+class UserData {
+  final String uid;
+  final String pizzaType;
+  final String name;
+  final String address;
+  final int size;
+  final bool extraCheese;
+
+  UserData(
+      {this.uid,
+      this.pizzaType,
+      this.name,
+      this.address,
+      this.size,
+      this.extraCheese});
+}
--- lib/services/databse.dart
+++ lib/services/databse.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firbase_example/modals/pizza.dart';
+import 'package:firbase_example/modals/user.dart';
 
 class DatabaseService {
 //Getting specific id
@@ -20,7 +21,18 @@ class DatabaseService {
     });
   }
 
-  // brew list from snapshot
+  //user data from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      address: snapshot.data['address'],
+      size: snapshot.data['size'],
+      extraCheese: snapshot.data['extraCheese'],
+    );
+  }
+
+  // pizza list from snapshot
   List<Pizza> _pizzaListFromSnapshot(QuerySnapshot snapshot) {
     return snapshot.documents.map((doc) {
       return Pizza(
@@ -36,5 +48,10 @@ class DatabaseService {
 //get pizza streem
   Stream<List<Pizza>> get pizza {
     return pizzaCollection.snapshots().map(_pizzaListFromSnapshot);
+  }
+
+  // get user doc stream
+  Stream<UserData> get userData {
+    return pizzaCollection.document(uid).snapshots().map(_userDataFromSnapshot);
   }
 }
