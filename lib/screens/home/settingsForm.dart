c74c4cb7117ce246027ed0e1c6d9daac16f158ca
yourgurunet <info@yourgurunet.com>
2021-01-07 10:02:08 +0200

    Added default values for setting modal

--- lib/screens/home/settingsForm.dart
+++ lib/screens/home/settingsForm.dart
@@ -1,5 +1,9 @@
+import 'package:firbase_example/modals/user.dart';
+import 'package:firbase_example/services/databse.dart';
 import 'package:firbase_example/shared/constants.dart';
+import 'package:firbase_example/shared/loading.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   @override
@@ -27,118 +31,133 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Container(
-        child: ListView(
-          children: [
-            Text(
-              'Update your pizza choice.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.blueGrey[400],
-                fontSize: 18,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              validator: (val) => val.isEmpty ? 'Enter your name' : null,
-              cursorColor: Colors.blueGrey[400],
-              decoration: textInputDecorations.copyWith(labelText: 'Name'),
-              obscureText: true,
-              onChanged: (val) {
-                setState(() => _currentName = val);
-              },
-            ),
-            TextFormField(
-              validator: (val) => val.isEmpty ? 'Enter your address' : null,
-              cursorColor: Colors.blueGrey[400],
-              decoration: textInputDecorations.copyWith(labelText: 'Address'),
-              obscureText: true,
-              onChanged: (val) {
-                setState(() => _currentAddress = val);
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            DropdownButtonFormField(
-              decoration: textInputDecorations,
-              items: pizzaTypes.map((pizzaType) {
-                return DropdownMenuItem(
-                  value: pizzaType,
-                  child: Text(
-                    pizzaType,
-                    style: TextStyle(
-                      color: Colors.blueGrey[400],
+    final user = Provider.of<User>(context);
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserData userData = snapshot.data;
+            return Form(
+              key: _formKey,
+              child: Container(
+                child: ListView(
+                  children: [
+                    Text(
+                      'Update your pizza choice.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.blueGrey[400],
+                        fontSize: 18,
+                        fontWeight: FontWeight.w900,
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-              onChanged: (val) {
-                setState(() => _currentPizzaType = val);
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text('Pizza size?',
-                style: TextStyle(
-                    color: Colors.blueGrey[400],
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400)),
-            Slider(
-              activeColor: Colors.blueGrey[400],
-              inactiveColor: Colors.blueGrey[200],
-              value: (_currentSize ?? 25).toDouble(),
-              min: 15,
-              max: 25,
-              divisions: 2,
-              label: _currentSize.round().toString(),
-              onChanged: (val) {
-                setState(() => _currentSize = val.round());
-              },
-            ),
-            Row(
-              children: [
-                Text(
-                  'Do you wanna extra cheese?',
-                  style: TextStyle(
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      initialValue: userData.name,
+                      validator: (val) =>
+                          val.isEmpty ? 'Enter your name' : null,
+                      cursorColor: Colors.blueGrey[400],
+                      decoration:
+                          textInputDecorations.copyWith(labelText: 'Name'),
+                      onChanged: (val) {
+                        setState(() => _currentName = val);
+                      },
+                    ),
+                    TextFormField(
+                      initialValue: userData.address,
+                      validator: (val) =>
+                          val.isEmpty ? 'Enter your address' : null,
+                      cursorColor: Colors.blueGrey[400],
+                      decoration:
+                          textInputDecorations.copyWith(labelText: 'Address'),
+                      onChanged: (val) {
+                        setState(() => _currentAddress = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    DropdownButtonFormField(
+                      value: _currentPizzaType ?? userData.pizzaType,
+                      decoration: textInputDecorations,
+                      items: pizzaTypes.map((pizzaType) {
+                        return DropdownMenuItem(
+                          value: pizzaType,
+                          child: Text(
+                            pizzaType,
+                            style: TextStyle(
+                              color: Colors.blueGrey[400],
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (val) {
+                        setState(() => _currentPizzaType = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('Pizza size?',
+                        style: TextStyle(
+                            color: Colors.blueGrey[400],
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400)),
+                    Slider(
+                      activeColor: Colors.blueGrey[400],
+                      inactiveColor: Colors.blueGrey[200],
+                      value: (_currentSize ?? userData.size).toDouble(),
+                      min: 15,
+                      max: 25,
+                      divisions: 2,
+                      label: _currentSize.round().toString(),
+                      onChanged: (val) {
+                        setState(() => _currentSize = val.round());
+                      },
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Do you wanna extra cheese?',
+                          style: TextStyle(
+                              color: Colors.blueGrey[400],
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Checkbox(
+                            value: extraCheese,
+                            checkColor: Colors.blueGrey[100],
+                            activeColor: Colors.blueGrey[400],
+                            onChanged: (bool value) {
+                              setState(() {
+                                extraCheese = value;
+                              });
+                            }),
+                      ],
+                    ),
+                    RaisedButton(
                       color: Colors.blueGrey[400],
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400),
+                      child: Text('Update',
+                          style: TextStyle(
+                            color: Colors.blueGrey[100],
+                          )),
+                      onPressed: () async {
+                        print(_currentAddress);
+                        print(_currentName);
+                        print(extraCheese);
+                        print(_currentPizzaType);
+                        print(_currentSize);
+                      },
+                    ),
+                  ],
                 ),
-                Checkbox(
-                    value: extraCheese,
-                    checkColor: Colors.blueGrey[100],
-                    activeColor: Colors.blueGrey[400],
-                    onChanged: (bool value) {
-                      setState(() {
-                        extraCheese = value;
-                      });
-                    }),
-              ],
-            ),
-            RaisedButton(
-              color: Colors.blueGrey[400],
-              child: Text('Update',
-                  style: TextStyle(
-                    color: Colors.blueGrey[100],
-                  )),
-              onPressed: () async {
-                print(_currentAddress);
-                print(_currentName);
-                print(extraCheese);
-                print(_currentPizzaType);
-                print(_currentSize);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+              ),
+            );
+          } else {
+            return Loading();
+          }
+        });
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -8,7 +8,7 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-    print(user);
+    // print(user);
 
     //Retun either Home or Athenticate wiged
     if (user == null) {
